b2ccf72a7c5c3274ef96a1fd57c8bf78ef286bc4
Mauro1642 <mauchacf@gmail.com>
2025-05-26 12:28:12 -0300

    Actualizado

--- Tp1.R
+++ Tp1.R
@@ -3,7 +3,7 @@ n<-nrow(datos) #cantidad de datos
 set.seed(123)
 #separacion 70%, 30% de los datos
 data_training<-datos[sample(1:n, size = 0.7 * n),]
-data_set<-datos[-sample(1:n, size = 0.7 * n),]
+data_test<-datos[-sample(1:n, size = 0.7 * n),]
 library(MASS)
 data_training$Q33 <- ordered(data_training$Q33, levels = c(1, 2, 3, 4, 5))
 modelo_Q33 <- polr(Q33 ~ age, data = data_training, method = "logistic", Hess = TRUE)
@@ -21,12 +21,28 @@ age<-25
 print(proba(theta,coeficiente,age,k))
 #Pregunta 7
 loss_function<-function(predichos,reales) {
+  pred_num <- as.numeric(predichos)
+  real_num <- as.numeric(reales)
   res=0
-  n<-length(reales)
+  n<-length(real_num)
   for(i in 1:n){
-    res=res+abs(predichos[i]-reales[i])
+    res=res+abs(pred_num[i]-real_num[i])
   }
   return(res/n)
   
 }
-modelo_Q9$coefficients
+#pregunta 8
+data_training$Q_num <- as.numeric(as.character(data_training$Q33))
+modelo_lineal <- lm(Q_num ~ age, data = data_training)
+#predigo para el modelo lineal
+predicciones_continuas <- predict(modelo_lineal, newdata = data_test)
+predicciones_finales <- pmin(pmax(round(predicciones_continuas), 1), 5)
+L_linear<-loss_function(predicciones_finales,data_test$Q33)
+#predigo para el modelo ordinal
+data_test$Q33 <- factor(data_test$Q33,
+                        levels = c("1", "2", "3", "4", "5"),
+                        ordered = TRUE)
+predicciones_ordinal<-predict(modelo_Q33,newdata=data_test)
+levels(predicciones_ordinal)
+levels(data_test$Q33)
+L_ordinal<-loss_function(predicciones_ordinal,data_test$Q33)
