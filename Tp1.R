1fa6efb596f7f1eded1394c3167f16d29f7350c5
Mauro1642 <mauchacf@gmail.com>
2025-05-27 14:22:52 -0300

    Ultimas modificaciones

--- Tp1.R
+++ Tp1.R
@@ -1,48 +1,44 @@
+library(MASS)
 datos<-read.csv("data.csv",sep="\t")
 n<-nrow(datos) #cantidad de datos
 set.seed(123)
 #separacion 70%, 30% de los datos
 data_training<-datos[sample(1:n, size = 0.7 * n),]
 data_test<-datos[-sample(1:n, size = 0.7 * n),]
-library(MASS)
-data_training$Q33 <- ordered(data_training$Q33, levels = c(1, 2, 3, 4, 5))
-modelo_Q33 <- polr(Q33 ~ age, data = data_training, method = "logistic", Hess = TRUE)
+data_training$Q2 <- ordered(data_training$Q2, levels = c(1, 2, 3, 4, 5)) #lo convierto en un factor ordenado
+modelo_Q2 <- polr(Q2 ~ age, data = data_training, method = "logistic", Hess = TRUE)
 #Pregunta 6
-data_training$Q9<-ordered(data_training$Q9, levels = c(1, 2, 3, 4, 5))
+data_training$Q9<-ordered(data_training$Q9, levels = c(1, 2, 3, 4, 5)) #lo convierto en un factor ordenado
 modelo_Q9 <- polr(Q9 ~ age, data = data_training, method = "logistic", Hess = TRUE)
 proba<-function(theta,coef,age,k){
   denominador<-1+exp(-(theta[k]-coef*age))
   return (1/denominador)
 }
+#Obtengo los parametros entrenados y luego calculo la probabilidad
 theta<-modelo_Q9$zeta
 coeficiente<-modelo_Q9$coefficients
-k<-4
+k<-3
 age<-25
-print(proba(theta,coeficiente,age,k))
+print(1-proba(theta,coeficiente,age,k))
 #Pregunta 7
-loss_function<-function(predichos,reales) {
-  pred_num <- as.numeric(predichos)
-  real_num <- as.numeric(reales)
-  res=0
-  n<-length(real_num)
-  for(i in 1:n){
-    res=res+abs(pred_num[i]-real_num[i])
-  }
-  return(res/n)
+loss_function <- function(predichos, reales) {
+  pred_num <- as.numeric(as.character(predichos)) #convierto los factores ordenados a numericos
+  real_num <- as.numeric(as.character(reales))
   
+  n <- length(real_num)
+  res <- sum(abs(pred_num - real_num)) / n
+  return(res)
 }
 #pregunta 8
-data_training$Q_num <- as.numeric(as.character(data_training$Q33))
-modelo_lineal <- lm(Q_num ~ age, data = data_training)
+data_training$Q2 <- as.numeric(as.character(data_training$Q2))
+modelo_lineal <- lm(Q2 ~ age, data = data_training)
 #predigo para el modelo lineal
 predicciones_continuas <- predict(modelo_lineal, newdata = data_test)
+predicciones_continuas
 predicciones_finales <- pmin(pmax(round(predicciones_continuas), 1), 5)
-L_linear<-loss_function(predicciones_finales,data_test$Q33)
+predicciones_finales
+L_linear<-loss_function(predicciones_finales,data_test$Q2)
 #predigo para el modelo ordinal
-data_test$Q33 <- factor(data_test$Q33,
-                        levels = c("1", "2", "3", "4", "5"),
-                        ordered = TRUE)
-predicciones_ordinal<-predict(modelo_Q33,newdata=data_test)
-levels(predicciones_ordinal)
-levels(data_test$Q33)
-L_ordinal<-loss_function(predicciones_ordinal,data_test$Q33)
+predicciones_ordinal<-predict(modelo_Q2,newdata=data_test)
+L_ordinal<-loss_function(predicciones_ordinal,data_test$Q2)
+predicciones_ordinal
